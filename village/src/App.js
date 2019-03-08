f81525b3a1ccbe9993650384302650a275ed4b45
tactnician <tactnician@gmail.com>
2019-03-08 13:06:54 -0500

    updated smurf form and refactored to manage state in App component

--- village/src/App.js
+++ village/src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -3,48 +3,55 @@ import React, { Component } from 'react';
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
+    this.state ={
+      smurf: {
+        name: '',
+        age: '',
+        height: ''
+      }
+    }
   }
 
-  addSmurf = event => {
-    event.preventDefault();
+  handleSubmit = e => {
+    e.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    this.props.addSmurf(e, this.state.smurf)
+    // this.setState({
+    //   name: '',
+    //   age: '',
+    //   height: ''
+    // });
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      smurf:{
+        ...this.state.smurf,
+        [e.target.name]: e.target.value
+      }
+    });
   };
 
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={this.handleSubmit}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={this.state.smurf.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={this.state.smurf.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={this.state.smurf.height}
             name="height"
           />
           <button type="submit">Add to the village</button>
